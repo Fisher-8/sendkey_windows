c2419b8f3f5a857a0b79df1ef3e1517825f893de
Fisher-8 <213799743+Fisher-8@users.noreply.github.com>
2025-07-20 13:38:51 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SendKey_Windows
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 40);
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top|System.Windows.Forms.AnchorStyles.Bottom)|System.Windows.Forms.AnchorStyles.Left)|System.Windows.Forms.AnchorStyles.Right)));
+            this.Name = "Form1";
+            this.Text = "F1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ClientSize = new System.Drawing.Size(104, 44);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ShowInTaskbar = false;
+            this.Controls.Add(this.textBox1);
+            this.Load += new System.EventHandler(this.fProgram_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fProgram_Close);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SendKey_Windows
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")]
+        static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vKey);
+
+        [DllImport("user32")]
+        static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fProgram_Load(object sender, EventArgs e)
+        {
+            RegisterHotKey(this.Handle, 31196, 0, 0x70);
+        }
+
+        private void fProgram_Close(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 31196);
+        }
+
+        char[] cArray = { '\n', '\r', '+', '^', '%', '~'};
+
+        private void InputKeys(string s)
+        {
+            foreach (char c in s)
+            {
+                var x = Array.FindIndex(cArray, i => i == c);
+                if ( x > -1 )
+                {
+                    switch (x)
+                    {
+                        //case 0: SendKeys.Send("{ENTER}"); break;
+                        case 1: SendKeys.Send("{ENTER}"); break;
+                        case 2: SendKeys.Send("{+}"); break;
+                        case 3: SendKeys.Send("{^}"); break;
+                        case 4: SendKeys.Send("{%}"); break;
+                        case 5: SendKeys.Send("{~}"); break;
+                    }
+                }
+                else
+                {
+                    SendKeys.Send(c.ToString());
+                }
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.WParam.ToInt32() == 31196)
+            {
+                InputKeys(textBox1.Text);
+            }
+            base.WndProc(ref m);
+        }
+    }
+}
