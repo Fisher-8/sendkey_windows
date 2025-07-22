74303d379d83f59b27ed6c60e74a5ee38bd8caad
Fisher-8 <213799743+Fisher-8@users.noreply.github.com>
2025-07-22 20:36:55 +0900

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,60 +14,43 @@ namespace SendKey_Windows
         [DllImport("user32")]
         static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        static string fnKey = "F1";
-        static int fnKeyNm = 112, enterDelay = 200, keyDelay = 10;
+        private string fnKey = "F1";
+        private int p0, p1, p2, fnKeyNm = 112, enterDelay = 300, keyDelay = 5;
+        private const int hKey = 0x0312, hKeyID = 31196;
+        private static readonly Dictionary<char, string> dKey = new Dictionary<char, string>() { ['+'] = "{+}", ['^'] = "{^}", ['%'] = "{%}", ['~'] = "{~}", ['('] = "{(}", [')'] = "{)}", ['{'] = "{{}", ['}'] = "{}}" };
 
         public Form1(string[] param)
         {
-            if (param.Length > 0) { int.TryParse(param[0], out fnKeyNm); if (fnKeyNm >= 1 && fnKeyNm <= 24) { fnKey = "F" + fnKeyNm.ToString(); fnKeyNm = +fnKeyNm + 111; } }
-            if (param.Length > 1) { int.TryParse(param[1], out enterDelay); if (!(enterDelay >= 0 && enterDelay <= 9999)) { enterDelay = 200; } }
-            if (param.Length > 2) { int.TryParse(param[2], out keyDelay); if (!(keyDelay >= 0 && keyDelay <= 999)) { keyDelay = 10; } }
+            if (param.Length > 0) { if (int.TryParse(param[0], out p0) && p0 >= 1 && p0 <= 24) { fnKey = "F" + p0.ToString(); fnKeyNm = p0 + 111; } }
+            if (param.Length > 1) { if (int.TryParse(param[1], out p1) && p1 >= 0 && p1 <= 9999) { enterDelay = p1; } }
+            if (param.Length > 2) { if (int.TryParse(param[2], out p2) && p2 >= 0 && p2 <= 999) { keyDelay = p2; } }
             InitializeComponent();
         }
 
         private void fProgram_Load(object sender, EventArgs e)
         {
-            RegisterHotKey(this.Handle, 31196, 0, (byte)fnKeyNm);
+            RegisterHotKey(this.Handle, hKeyID, 0, (byte)fnKeyNm);
         }
 
         private void fProgram_Close(object sender, FormClosingEventArgs e)
         {
-            UnregisterHotKey(this.Handle, 31196);
+            UnregisterHotKey(this.Handle, hKeyID);
         }
 
-        char[] cArray = { '\n', '\r', '+', '^', '%', '~', '(', ')', '{', '}'};
-
-        private void InputKeys(string s)
+        private async void InputKeys(string s)
         {
             foreach (char c in s)
             {
-                var x = Array.FindIndex(cArray, i => i == c);
-                if ( x > -1 )
-                {
-                    switch (x)
-                    {
-                        //case 0: SendKeys.Send("{ENTER}"); break;
-                        case 1: SendKeys.SendWait("{ENTER}"); Task.Delay(enterDelay).Wait(); break;
-                        case 2: SendKeys.SendWait("{+}"); Task.Delay(keyDelay).Wait(); break;
-                        case 3: SendKeys.SendWait("{^}"); Task.Delay(keyDelay).Wait(); break;
-                        case 4: SendKeys.SendWait("{%}"); Task.Delay(keyDelay).Wait(); break;
-                        case 5: SendKeys.SendWait("{~}"); Task.Delay(keyDelay).Wait(); break;
-                        case 6: SendKeys.SendWait("{(}"); Task.Delay(keyDelay).Wait(); break;
-                        case 7: SendKeys.SendWait("{)}"); Task.Delay(keyDelay).Wait(); break;
-                        case 8: SendKeys.SendWait("{{}"); Task.Delay(keyDelay).Wait(); break;
-                        case 9: SendKeys.SendWait("{}}"); Task.Delay(keyDelay).Wait(); break;
-                    }
-                }
-                else
-                {
-                    SendKeys.Send(c.ToString()); Task.Delay(keyDelay).Wait();
-                }
+                if (dKey.TryGetValue(c, out var k)) { SendKeys.SendWait(k); await Task.Delay(keyDelay); }
+                else if (c == '\n') { }
+                else if (c == '\r') { SendKeys.SendWait("{ENTER}"); await Task.Delay(enterDelay); }
+                else { SendKeys.SendWait(c.ToString()); await Task.Delay(keyDelay); }
             }
         }
 
         protected override void WndProc(ref Message m)
         {
-            if (m.WParam.ToInt32() == 31196)
+            if (m.Msg == hKey && m.WParam.ToInt32() == hKeyID)
             {
                 InputKeys(textBox1.Text);
             }
